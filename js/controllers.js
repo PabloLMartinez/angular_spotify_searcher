a864dcad19d94d1b3883820a8a44351afa39daa2
Pablo Lucas Martinez <pablo.martinez152@gmail.com>
2018-04-01 23:49:23 -0300

    Documenting file

--- js/controllers.js
+++ js/controllers.js
@@ -5,18 +5,28 @@ angular.module('SpotifySearcher.controllers', [])
     .controller('SpotifySearcherIndexCtrl', function ($scope, $rootScope, SpotifySearcherService) {
 
         $scope.searchKey = "";
-
+    
+        /**
+         * Function clear input search
+         */
         $scope.clearSearch = function () {
             $scope.searchKey = "";
             $scope.tracks = "";
         }
 
+        /**
+         * Function search by name
+         */
         $scope.search = function () {
             SpotifySearcherService.findByTrackName($scope.searchKey).then(function (tracks) {
                 $scope.tracks = tracks.tracks.items;
             });
         }
 
+        /**
+         * Function add favourite track
+         * @param {object} track
+         */
         $scope.addToFavourites = function (track) {
             $rootScope.favouritesTracks.push(track.name);
 
@@ -29,6 +39,10 @@ angular.module('SpotifySearcher.controllers', [])
 
         $rootScope.favouritesTracks = JSON.parse(localStorage.getItem('favouritesTracks'));
 
+        /**
+         * Function remove a favourite track
+         * @param {object} item
+         */
         $scope.removeTrack = function (item) {
 
             $rootScope.favouritesTracks.splice(item, 1);

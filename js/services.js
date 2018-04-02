059fb2abaaf150d6087a3dcd55001a9c59f1679b
Pablo Lucas Martinez <pablo.martinez152@gmail.com>
2018-04-01 23:52:49 -0300

    bug fixing

--- js/services.js
+++ js/services.js
@@ -3,10 +3,7 @@ angular.module('SpotifySearcher.services', [])
     .factory('SpotifySearcherService', function($http, $q) {
 
         return {
-            /**
-             * Function for searching a track by name
-             * @param {string} searchKey
-             */
+            
             findByTrackName: function(searchKey) {
                 var searchTrack = searchKey.replace(' ', '+');
                 var deferred = $q.defer();
